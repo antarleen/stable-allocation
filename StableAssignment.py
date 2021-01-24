c4dfa8d8c750eb1133d8a35760d6a5563177e26d
Antarleen <76465119+antarleen@users.noreply.github.com>
2021-01-24 20:52:19 +0530

    Update StableAssignment.py

--- StableAssignment.py
+++ StableAssignment.py
@@ -1,26 +1,35 @@
 def group_strength_checker_pref():
     for i in range(1, 31):
         while True:
-            gs = int(input(f'Hi ! Group {i}, Please enter your group strength:'))
-            if gs <= 3 and gs != 0:
-                print('Your group has been accepted !')
-                print('Please enter 5 mentor names as preference:')
-                for j in range(5):
-                    while True:
-                        choice = int(input('Please enter a mentor Number:\n101 for A\n102 for B\n103 for C\n104 for '
-                                           'D\n105 for E\n106 for F\n107 for G\n108 for H\n109 for I\n110 for I\n111 '
-                                           'for J\n112 '
-                                           'for K\n113 for L\n114 for M\n115 for N'))
-                        if choice not in group_mentor_preference[i - 1]:
-                            group_mentor_preference[i - 1][j] = choice
-                            break
-                        else:
-                            print('Mentor already selected, pick a different mentor !')
+            try:
+                gs = int(input(f'Hi ! Group {i}, Please enter your group strength:'))
+                if gs <= 3 and gs != 0:
+                    print('Your group has been accepted !')
+                    print('Please enter 5 mentor names as preference:')
+                    for j in range(5):
+                        while True:
+                            try:
+                                choice = int(
+                                    input('Please enter a mentor Number:\n101 for A\n102 for B\n103 for C\n104 '
+                                          'for '
+                                          'D\n105 for E\n106 for F\n107 for G\n108 for H\n109 for I\n110 for '
+                                          'I\n111 '
+                                          'for J\n112 '
+                                          'for K\n113 for L\n114 for M\n115 for N\n'))
+                                if choice not in group_mentor_preference[i - 1]:
+                                    group_mentor_preference[i - 1][j] = choice
+                                    break
+                                else:
+                                    print('Mentor already selected, pick a different mentor !')
+                            except ValueError:
+                                print('Not a valid input !! Please enter again')
 
-                group_strength.append(gs)
-                break
-            else:
-                print('Group must have between 1 and 3 people, please enter again !')
+                    group_strength.append(gs)
+                    break
+                else:
+                    print('Group must have between 1 and 3 people, please enter again !')
+            except ValueError:
+                print('Please enter a valid input !!')
 
 
 def mentor_preference():
@@ -28,12 +37,15 @@ def mentor_preference():
         print(f'Hi! Mentor {100 + i} !')
         for j in range(3):
             while True:
-                choice = int(input('Please enter a group Number as per preference:'))
-                if choice not in mentor_group_preference[i - 1]:
-                    mentor_group_preference[i - 1][j] = choice
-                    break
-                else:
-                    print('Group already selected, pick a different group !')
+                try:
+                    choice = int(input('Please enter a group Number as per preference:'))
+                    if choice not in mentor_group_preference[i - 1]:
+                        mentor_group_preference[i - 1][j] = choice
+                        break
+                    else:
+                        print('Group already selected, pick a different group !')
+                except ValueError:
+                    print('Not a valid input !! Please enter again')
 
 
 if __name__ == '__main__':
@@ -58,7 +70,8 @@ if __name__ == '__main__':
             while len(mentor_dict[key]) > 3:
                 lowest_pref = mentor_dict[key][0]
                 for group in mentor_dict[key]:
-                    if mentor_group_preference[int(key % 100) - 1].index(lowest_pref) < mentor_group_preference[int(key % 100) - 1].index(group):
+                    if mentor_group_preference[int(key % 100) - 1].index(lowest_pref) < mentor_group_preference[
+                            int(key % 100) - 1].index(group):
                         lowest_pref = group
                     mentor_dict[key].remove(lowest_pref)
 
@@ -69,4 +82,4 @@ if __name__ == '__main__':
     for keys in mentor_dict:
         print(f'\nMENTOR {keys} HAS BEEN ALLOCATED THE FOLLOWING GROUPS:\n')
         for groups in keys:
-            print(groups+'\n')
+            print(groups + '\n')
